ac28d99ea44624134570a4c468b8958d6a40d0e0
Paul Colomiets <paul@colomiets.name>
2016-04-13 16:57:11 +0300

    Add integer(level) metrics support

--- examples/counters.go
+++ examples/counters.go
@@ -2,9 +2,13 @@ package main
 import cantal "github.com/tailhook/cantal-go"
 import "fmt"
 import "time"
+import "math/rand"
 
 var counter = cantal.NewCounter(map[string]string{
     "metric": "dots_printed",
+    })
+var integer = cantal.NewInteger(map[string]string{
+    "metric": "random_value",
     })
 
 func main() {
@@ -13,6 +17,7 @@ func main() {
 
     for true {
         counter.Incr()
+        integer.Set(10 + rand.Int63() % 100)
         time.Sleep(100*time.Millisecond)
         fmt.Print(".")
     }
---
+++ integer.go
@@ -0,0 +1,42 @@
+package cantal
+
+import "sync/atomic"
+import "unsafe"
+
+
+type Integer struct {
+    name map[string]string
+    value *int64
+}
+
+func NewInteger(name map[string]string) *Integer {
+    counter := new(Integer)
+    *counter = Integer {
+        name: name,
+        value: new(int64), // will be replaced later
+    }
+    add_value(counter)
+    return counter
+}
+
+func (*Integer) GetSize() int {
+    return 8
+}
+
+func (*Integer) GetType() string {
+    return "counter"
+}
+
+func (self*Integer) Set(value int64) {
+    // This isn't strictly needed, at least for x86_64
+    // But we think it makes code more clear
+    atomic.StoreInt64(self.value, value)
+}
+
+func (self*Integer) GetName() *map[string]string {
+    return &self.name
+}
+
+func (self*Integer) set_pointer(ptr unsafe.Pointer) {
+    self.value = (*int64)(ptr)
+}
